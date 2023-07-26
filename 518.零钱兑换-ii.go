8770dc73f30852263a4c518bce9de8983fa1e940
栗山未来 <1780759836@qq.com>
2023-07-26 19:33:29 +0800

    2023/7/26

---
+++ 518.零钱兑换-ii.go
@@ -0,0 +1,23 @@
+package leetcode
+
+/*
+ * @lc app=leetcode.cn id=518 lang=golang
+ *
+ * [518] 零钱兑换 II
+ */
+
+// @lc code=start
+func change(amount int, coins []int) int {
+	dp := make([]int, amount+1)
+	dp[0] = 1
+
+	for i := 0; i < len(coins); i++ {
+		for j := coins[i]; j <= amount; j++ {
+			dp[j] += dp[j-coins[i]]
+		}
+	}
+
+	return dp[amount]
+}
+
+// @lc code=end
